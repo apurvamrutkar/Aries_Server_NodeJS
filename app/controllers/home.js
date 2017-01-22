5e67470e79def6427adc3cc1bf1a9ed04f9f839f
Apurv Amrutkar <Apurv Amrutkar>
2017-01-22 03:32:22 -0500

    removing items from shopping list after ordering

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -71,7 +71,7 @@ exports.addToFridge = function(req, res){
 		res.status(403).json({error:'Barcodes incorrect'});
 		return;
 	}else{
-		console.log("add to fridge:"+req.body.itemList);
+		console.log("add to fridge:"+JSON.stringify(req.body.itemList));
 		Family.findById(req.params.id, function(err, Family){
 			if (err) {console.log(err);res.status(500).json({error:'Family not found'});}
 			if(Family!=null){
@@ -256,7 +256,7 @@ exports.addToGarbage = function(req, res){
 	if(req.body.itemList==null || req.body.itemList.length==0){
 		res.status(403).json({error:'Barcodes incorrect'});
 	}else{
-		console.log("add to garbage:"+req.body.itemList);
+		console.log("add to garbage:"+JSON.stringify(req.body.itemList));
 		Family.findById(req.params.id, function(err, Family){
 			if (err) {console.log(err);res.status(500).json({error:'Family not found'});}
 			if(Family!=null){
